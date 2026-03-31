0bdb2dbe83cdd9827cd8c409928a9a4c519545df
Jerome <jeromepacaud39@gmail.com>
2026-03-31 13:41:13 +0200

    feat: add Classroom class to manage a list of users and merge Classroom

--- app/main.py
+++ app/main.py
@@ -1,3 +1,46 @@
-def add(a: int, b: int) -> int:
-    """Retourne la somme de deux entiers."""
-    return a + b
+from models import Classroom, Learner, User
+
+if __name__ == '__main__':
+    user = User(name='Alice', id=1)
+    learner_1 = Learner(name='Bob', id=2)
+    learner_2 = Learner(name='Charlie', id=3)
+    learner_3 = Learner(name='David', id=4)
+    learner_4 = Learner(name='Eve', id=5)
+
+    # User tests
+    print(user.name)
+    print(user.id)
+    print(user.can_validate(skill_id=1))
+
+    # Learner tests
+    print(learner_1.name)
+    print(learner_1.id)
+    print(learner_1.can_validate(skill_id=1))
+    learner_1.add_skill(skill_id=1)
+    print(learner_1.can_validate(skill_id=1))
+    print(learner_1)
+    print(learner_2)
+    print(learner_3)
+    print(learner_4)
+
+    # Classroom tests
+    classroom_1 = Classroom('P1')
+    classroom_2 = Classroom('P2')
+    classroom_3 = Classroom('P32')
+    classroom_4 = Classroom('P28')
+    print(classroom_1.name)
+    classroom_1.add_user(learner_1)
+    classroom_1.add_user(learner_2)
+
+    print(classroom_2.name)
+    classroom_2.add_user(learner_3)
+    classroom_2.add_user(learner_4)
+
+    p3 = classroom_1 + classroom_2
+    print(p3)
+    print(p3.get_users())
+
+    p4 = classroom_3 + classroom_4
+    print(p4)
+
+
--- app/models.py
+++ app/models.py
@@ -1,39 +1,22 @@
 class User:
-    """
-    Représente un utilisateur du système.
-
-    Attributes:
-        name (str) : Le nom de l'utilisateur.
-        id (int) : L'identifiant unique de l'utilisateur.
-    """
-
     def __init__(self, name: str, id: int) -> None:
-        """
-        Initialise un nouvel utilisateur avec un nom et un identifiant.
-
-        Args:
-            name (str): Le nom de l'utilisateur.
-            id (int): L'identifiant unique de l'utilisateur.
-        """
-        self.name = name
-        self.id = id
+        self.name: str = name
+        self.id: int = id
 
     def can_validate(self, skill_id: int) -> bool:
-        """
-        Indique si l'utilisateur peut valider une compétence spécifique.
-
-        Args:
-            skill_id (int) : L'identifiant de la compétence à vérifier.
-
-        Returns:
-            bool: Toujours False pour l'instant.
-        """
         return False
+
+    def __str__(self) -> str:
+        return f'Utilisateur : {self.id} | {self.name}'
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
 
 class Learner(User):
     def __init__(self, name: str, id: int) -> None:
         super().__init__(name, id)
-        self._validated_skills = []
+        self._validated_skills: list[int] = []
 
     def can_validate(self, skill_id: int) -> bool:
         return skill_id in self._validated_skills
@@ -43,6 +26,40 @@ class Learner(User):
             raise ValueError(f'La compétences {skill_id} est déja validée !')
         self._validated_skills.append(skill_id)
 
+    def __str__(self) -> str:
+        return f'Apprenant : {self.id} | {self.name}'
+
+
 class Trainer(User):
     def can_validate(self, skill_id: int) -> bool:
         return True
+
+    def __str__(self) -> str:
+        return f'Formateur : {self.id} | {self.name}'
+
+
+class Classroom:
+    def __init__(self, name: str) -> None:
+        self.name: str = name
+        self._users: list[User] = []
+
+    def get_users(self) -> list[User]:
+        return self._users
+
+    def add_user(self, user: User) -> None:
+        self._users.append(user)
+
+    def __add__(self, other: Classroom) -> Classroom:
+        merged_classroom = Classroom(
+            f'{self.name} + {other.name} => P{int(self.name[1:]) + int(other.name[1:])}'
+        )
+
+        for user in self._users:
+            merged_classroom.add_user(user)
+        for user in other._users:
+            merged_classroom.add_user(user)
+
+        return merged_classroom
+
+    def __str__(self) -> str:
+        return f'Promotion : {self.name} | {len(self._users)} users'
