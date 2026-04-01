5a403f18cbdb2171f018c4634bba3dd4fb72ad0b
Jerome Pacaud <j.pacaud.formation@protonmail.com>
2026-04-01 11:06:28 +0200

    fix: fix ambiguous variable name in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,61 +1,60 @@
 import pytest
-
 from models import (
-    Classroom,
-    Learner,
-    Skill,
-    Trainer,
-    User,
-    Validation,
-    ValidationStatus,
+	Classroom,
+	Learner,
+	Skill,
+	Trainer,
+	User,
+	Validation,
+	ValidationStatus,
 )
 
 
 @pytest.fixture
 def user() -> User:
-    return User(name="Alice", id=1)
+	return User(name='Alice', id=1)
 
 
 @pytest.fixture
 def learner() -> Learner:
-    return Learner(name="Bob", id=2)
+	return Learner(name='Bob', id=2)
 
 
 @pytest.fixture
 def learner_with_skill() -> Learner:
-    l = Learner(name="Bob", id=2)
-    l.add_skill(1)
-    return l
+	learner = Learner(name='Bob', id=2)
+	learner.add_skill(1)
+	return learner
 
 
 @pytest.fixture
 def trainer() -> Trainer:
-    return Trainer(name="John", id=99)
+	return Trainer(name='John', id=99)
 
 
 @pytest.fixture
 def classroom() -> Classroom:
-    return Classroom(name="P1")
+	return Classroom(name='P1')
 
 
 @pytest.fixture
 def classroom_with_users(learner: Learner, trainer: Trainer) -> Classroom:
-    c = Classroom(name="P1")
-    c.add_user(learner)
-    c.add_user(trainer)
-    return c
+	classroom = Classroom(name='P1')
+	classroom.add_user(learner)
+	classroom.add_user(trainer)
+	return classroom
 
 
 @pytest.fixture
 def skill() -> Skill:
-    return Skill(id=1, name="Python")
+	return Skill(id=1, name='Python')
 
 
 @pytest.fixture
 def validation() -> Validation:
-    return Validation(
-        id=1,
-        learner_id=10,
-        skill_id=1,
-        status=ValidationStatus.PENDING,
-    )
+	return Validation(
+		id=1,
+		learner_id=10,
+		skill_id=1,
+		status=ValidationStatus.PENDING,
+	)
